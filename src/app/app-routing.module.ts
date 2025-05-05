0ad3e349adc833225a194fe241a8783d5a6432ab
nawal-ali <nmaaa3899@gmail.com>
2025-05-05 10:04:52 +0300

    added navbar and done its routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SchoolsComponent } from './pages/schools/schools.component';
+import { IndividualsComponent } from './pages/individuals/individuals.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'schools',component:SchoolsComponent},
+  {path:'individuals',component:IndividualsComponent},
+  {path:'about',component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { SchoolsComponent } from './pages/schools/schools.component';
+import { IndividualsComponent } from './pages/individuals/individuals.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    SchoolsComponent,
+    IndividualsComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
